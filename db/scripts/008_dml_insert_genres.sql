f1a6658266b1b802a0218df514c0c5c8ecf387c7
Boris <boris86msk@yandex.ru>
2023-09-19 12:37:04 +0300

    add database

---
+++ db/scripts/008_dml_insert_genres.sql
@@ -0,0 +1,7 @@
+insert into genres(name) values('боевик');
+insert into genres(name) values('детектив');
+insert into genres(name) values('драмма');
+insert into genres(name) values('комедия');
+insert into genres(name) values('мелодрамма');
+insert into genres(name) values('приключения');
+insert into genres(name) values('фантастика');
